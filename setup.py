acf4485e47ff74c62281b439c2b9f77ba3d731db
Aramir94 <jeonghyunsuk777@gmail.com>
2023-11-17 15:55:27 +0900

    setup

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='sleeper',
-    version='0.1',
+    name='cute_sleeper',
+    version='0.2',
     packages=find_packages(),
     description='A simple Python library for random sleep times based on normal distribution',
     long_description=open('README.md').read(),
@@ -11,8 +11,5 @@ setup(
     author_email='jhs941222@naver.com',
     url='https://github.com/Aramir94/sleeper',
     license='Apache',
-    install_requires=[
-        'random',
-        'time'
-    ]
+    install_requires=[]
 )
